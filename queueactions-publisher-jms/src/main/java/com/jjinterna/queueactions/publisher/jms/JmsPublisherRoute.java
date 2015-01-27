56b3912983c2b0af9101fe840ca6ba24d38913bc
Georgi Manev <gmanev@gmail.com>
2015-01-27 17:47:50 +0200

    update

---
+++ queueactions-publisher-jms/src/main/java/com/jjinterna/queueactions/publisher/jms/JmsPublisherRoute.java
@@ -0,0 +1,52 @@
+package com.jjinterna.queueactions.publisher.jms;
+
+import java.util.List;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.CsvDataFormat;
+import org.apache.camel.model.dataformat.JaxbDataFormat;
+
+import com.jjinterna.queueactions.model.QueueEvent;
+import com.jjinterna.queueactions.model.QueueEventType;
+
+public class JmsPublisherRoute extends RouteBuilder {
+
+	@Override
+	public void configure() throws Exception {
+		
+		JaxbDataFormat jaxb = new JaxbDataFormat();
+		jaxb.setContextPath(QueueEvent.class.getPackage().getName());
+		jaxb.setPartClass("com.jjinterna.queueactions.model.QueueEvent");
+
+		from("stream:file?fileName=/tmp/queue_log").id("queue_log2jms")
+		.unmarshal(new CsvDataFormat("|"))
+		.process(new Processor() {			
+			@Override
+			public void process(Exchange exchange) throws Exception {
+		        Message in = exchange.getIn();
+		        List<List<String>> csvData = (List<List<String>>) in.getBody();
+		        List<String> fields = csvData.get(0);
+		        for (int i = fields.size(); i<10; i++) {
+		        	fields.add("");
+		        }
+		        in.setBody(new QueueEvent(
+		        		Integer.parseInt(fields.get(0)),
+		        		fields.get(1),
+		        		fields.get(2),
+		        		fields.get(3),
+		        		QueueEventType.fromValue(fields.get(4)),
+		        		fields.get(5),
+		        		fields.get(6),
+		        		fields.get(7),
+		        		fields.get(8),
+		        		fields.get(9)));
+			}
+		})
+		//.marshal(jaxb)
+		.to("jms:topic:QueueActions");
+	}
+
+}
